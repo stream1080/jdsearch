4651722924caf1ca7bbb8b9510448206e7b72ea1
stream1080 <fmxhax638@foxmail.com>
2021-10-07 10:42:50 +0800

    first commit

---
+++ src/main/java/com/stream/jdsearch/JdsearchApplication.java
@@ -0,0 +1,13 @@
+package com.stream.jdsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdsearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stream/jdsearch/JdsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stream.jdsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
